99838836eb048bb82dc457a7d44ce14425f190f8
web3anand <daybot61@gmail.com>
2025-09-27 12:58:05 +0530

    update domain

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,4 +1,4 @@
-const ROOT_URL = process.env.NEXT_PUBLIC_ROOT_URL || 'https://your-app.vercel.app';
+const ROOT_URL = process.env.NEXT_PUBLIC_ROOT_URL || 'https://xo-chi.vercel.app';
 
 export const minikitConfig = {
   accountAssociation: {
